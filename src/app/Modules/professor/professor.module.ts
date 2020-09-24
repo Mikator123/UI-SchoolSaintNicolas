d567449425c56b3023d483eef59d5d665e6ef11d
mikatournay@gmail.com <mikatournay@gmail.com>
2020-09-24 13:54:21 +0200

    result update WORKS !

--- src/app/Modules/professor/professor.module.ts
+++ src/app/Modules/professor/professor.module.ts
@@ -1,27 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProfessorRoutingModule } from './professor-routing.module';
 import { StudentListComponent } from './Components/student-list/student-list.component';
-import {MatCardModule} from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
 import { TrimestrialNoteComponent } from './Components/trimestrial-note/trimestrial-note.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { CreateComponent } from './Components/trimestrial-note/create/create.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
 import { TestResultComponent } from './Components/test-result/test-result.component';
-import { MatMenuModule } from '@angular/material/menu';
 import { UserModule } from '../User/user.module';
 import { SharedModule } from '../shared/shared.module';
-import { UpdateComponent } from './Components/trimestrial-note/update/update.component';
+import { UpdateNoteComponent } from './Components/trimestrial-note/Update-note/update.component';
+import { CreateNoteComponent } from './Components/trimestrial-note/Create-note/create.component';
+import {UpdateResultComponent} from './Components/test-result/update-result/update-result.component';
+import {CreateResultComponent} from './Components/test-result/create-result/create-result.component';
+
+
 
 
 
@@ -32,9 +22,11 @@ import { UpdateComponent } from './Components/trimestrial-note/update/update.com
   declarations: [
     StudentListComponent,
     TrimestrialNoteComponent,
-    CreateComponent,
+    CreateNoteComponent,
+    UpdateNoteComponent,
     TestResultComponent,
-    UpdateComponent
+    UpdateResultComponent,
+    CreateResultComponent,
   ],
 
   imports: [
@@ -42,12 +34,17 @@ import { UpdateComponent } from './Components/trimestrial-note/update/update.com
     ProfessorRoutingModule,
     UserModule,
     SharedModule,
+    
   ],
 
   exports:[
     StudentListComponent,
     TrimestrialNoteComponent,
-    UpdateComponent,
+    CreateNoteComponent,
+    TestResultComponent,
+    UpdateNoteComponent,
+    UpdateResultComponent,
+    CreateResultComponent,
   ]
 })
 export class ProfessorModule { }
